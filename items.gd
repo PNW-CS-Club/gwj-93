20c55185f0e4cc0ab388debde55dec557b83b26b
Jeremy <jeremyorozco38@gmail.com>
2026-05-09 14:34:19 -0500

    Revisited Another Tutorial
    Was able to get the layout of the shop will type the code soon

--- items.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-class_name Item
-
-@export var itemName: String
-@export var texture: CompressedTexture2D
--- sell_plants.gd
+++ sell_plants.gd
@@ -1,5 +1,7 @@
-extends Node2D
+extends Control
+class_name SellPlants
 
+@export var plant_frame: TextureRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,9 +9,5 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	pass
-
-
-func _on_shop_button_toggled(toggled_on: bool) -> void:
-	pass # Replace with function body.
