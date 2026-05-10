3dcb5b20dec9899fdda1237c4db3a90bc546d924
Julian <sahaguj@pnw.edu>
2026-05-10 14:07:48 -0500

    Add documentation for attacks

--- scripts/enemy_attack.gd
+++ scripts/enemy_attack.gd
@@ -1,20 +1,23 @@
 class_name EnemyAttack extends Node2D
 
-var grid: Grid
-var rng = RandomNumberGenerator.new()
-
 signal squares_to_attack(marked_squares: Array[Array])
 
 enum Attacks {SQUARE, ROW_COLUMN, RANDOM}
 
 var guaranteed_hit_chance: float = 0.10: set = on_guaranteed_hit_chance_set
-var damage: int
+var grid: Grid # this should be a reference of the grid in the game
+var rng
 
+func _read():
+	rng = RandomNumberGenerator.new()
+
+## Set the guaranteed hit chance of the attack bound between [0.0,1.0].
 func on_guaranteed_hit_chance_set(amount: float) -> void:
 	guaranteed_hit_chance = clamp(amount, 0.0, 1.0)
 	
 
-
+## Attack function that can be used to specify the attack and the size of the attack.
+## Emits a signal of the marked squares
 func attack(type: Attacks = Attacks.SQUARE, size: int = 1) -> void:
 	# check if there are any plants first
 	if grid.plants.is_empty():
@@ -24,33 +27,31 @@ func attack(type: Attacks = Attacks.SQUARE, size: int = 1) -> void:
 	var guaranteed_hit: bool = rand_num <= guaranteed_hit_chance 
 	match type:
 		Attacks.SQUARE:
-			square_attack(guaranteed_hit, size)
+			squares_to_attack.emit( _square_attack(guaranteed_hit, size) )
 		Attacks.ROW_COLUMN:
-			row_column_attack(guaranteed_hit) # row column attack doesnt have a size
+			squares_to_attack.emit( _row_column_attack(guaranteed_hit) ) # row column attack doesnt have a size
 		Attacks.RANDOM:
-			random_attack(guaranteed_hit, size)
-
+			squares_to_attack.emit( _random_attack(guaranteed_hit, size) )
+	
 
 #region attacks
-func square_attack(guaranteed_hit: bool, size: int) -> void:
+## A square attack pattern where the size parameter determines the side length of the square. [br]
+## | 1 | 2 | 0 | [br]
+## | 3 | 4 | 0 | [br]
+## | 0 | 0 | 0 | [br]
+func _square_attack(guaranteed_hit: bool, size: int) -> Array[Array]:
 	var attack_length: int = clampi(size,1,grid.length) # Clamp size to grid length
 	attack_length = clampi(attack_length,1,grid.width)  # Clamp size to grid height
 	var column: int
 	var row: int
 	var marked_squares: Array[Array]
-	# _____________
-	# | 1 | 2 |   | 
-	# | 3 | 4 |   |
-	# |   |   |   |
-	# -------------
 	if guaranteed_hit: # generate the attack to hit at least 1 plant
 		var plant_coords: Array[int] = grid.plants.pick_random() # picks a random plant from the list of living plants
 		var plant_x: int = plant_coords[0]
 		var plant_y: int = plant_coords[1]
 		if attack_length == 1: # attack a single square
 			marked_squares.append(plant_coords)
-			squares_to_attack.emit(marked_squares)
-			return
+			return marked_squares
 		# else: generate the attack to hit the random plant
 		# find column start index
 		if (plant_x + attack_length) > (grid.length - 1):
@@ -68,31 +69,30 @@ func square_attack(guaranteed_hit: bool, size: int) -> void:
 		for r in range(row, row + attack_length):
 			for c in range(column, column + attack_length):
 				marked_squares.append([c,r])
-		squares_to_attack.emit(marked_squares)
-		return
+		return marked_squares
 	# else: generate the attack in a random spot in the grid
 	column = rng.randi_range(0,grid.length - attack_length)
 	row = rng.randi_range(0,grid.height - attack_length)
 	if attack_length == 1:
 		marked_squares.append([column,row])
-		squares_to_attack.emit(marked_squares)
-		return
+		return marked_squares
 	for r in range(row, row + attack_length):
 		for c in range(column, column + attack_length):
 			marked_squares.append([c,r])
-	squares_to_attack.emit(marked_squares)
+	return marked_squares
 
 
-func row_column_attack(guaranteed_hit: bool) -> void:
+## An Attack pattern that hits an entire row and column.
+## Attack row first then attack column. 
+## The square where the attacks overlap gets hit twice. [br]
+## | 1 |2 4| 3 | [br]
+## | 0 | 5 | 0 | [br]
+## | 0 | 6 | 0 | [br]
+func _row_column_attack(guaranteed_hit: bool) -> Array[Array]:
 	var column: int 
 	var row: int 
 	var marked_squares: Array[Array]
-	# Attack row first then attack column.
-	# _____________
-	# | 1 |2 4| 3 | # The square where the attacks overlap gets hit twice
-	# |   | 5 |   | 
-	# |   | 6 |   |
-	# -------------
+	
 	if guaranteed_hit: # Center the attack on the row and column of a random plant.
 		var plant_coords: Array[int] = grid.plants.pick_random() # picks a random plant from the list of living plants
 		var plant_x: int = plant_coords[0]
@@ -103,8 +103,7 @@ func row_column_attack(guaranteed_hit: bool) -> void:
 			marked_squares.append([c,row])
 		for r in range(grid.height): # Secondly mark the column.
 			marked_squares.append([column,r])
-		squares_to_attack.emit(marked_squares)
-		return
+		return marked_squares
 	# Generate a random column and random row.
 	column = rng.randi_range(0,grid.length-1)
 	row = rng.randi_range(0,grid.height-1)
@@ -112,17 +111,16 @@ func row_column_attack(guaranteed_hit: bool) -> void:
 		marked_squares.append([c,row])
 	for r in range(grid.height): # Secondly mark the column.
 		marked_squares.append([column,r])
-	squares_to_attack.emit(marked_squares)
+	return marked_squares
 
-
-func random_attack(guaranteed_hit: bool, size: int) -> void:
+## A random attack where size is the amount of hits. [br]
+## | 2 | 1 |[br]
+## | 3 | 0 |[br]
+## | 0 | 4 |[br]
+func _random_attack(guaranteed_hit: bool, size: int) -> Array[Array]:
 	# size in this function is the amount of random attacks
 	var marked_squares: Array[Array]
-	# _____________
-	# | 2 | 1 |   | 
-	# | 3 |   |   |
-	# |   |   | 4 |
-	# -------------
+	
 	if guaranteed_hit: # The very first strike is a guaranteed hit on a random plant.
 		var plant_coords: Array[int] = grid.plants.pick_random() # picks a random plant from the list of living plants
 		marked_squares.append(plant_coords)
@@ -130,5 +128,5 @@ func random_attack(guaranteed_hit: bool, size: int) -> void:
 	# The remaining attacks are random
 	for i in range(size):
 		marked_squares.append([rng.randi_range(0,grid.length), rng.randi_range(0,grid.height)])
-	squares_to_attack.emit(marked_squares)
+	return marked_squares
 #endregion
