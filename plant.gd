a2a51209c09079d36f6643e5872ac738bda48282
Julian <sahaguj@pnw.edu>
2026-05-10 19:03:19 -0500

    Add Plant abstract class

--- plant.gd
+++
@@ -1,1 +0,0 @@
-class_name Plant extends Node
--- plants/defense/defense_plant.gd
+++ plants/defense/defense_plant.gd
@@ -1,4 +1,11 @@
 class_name DefensePlant extends Plant
-
-
 @export var stats: Stats
+
+func _ready():
+	%HealthBar.value = stats.base_max_health
+
+func _process(delta: float) -> void:
+	%HealthBar.value = stats.health
+
+func ability():
+	pass
