b77ad5736102d8f2bd3288739dbf12100d77b3bd
Daniel-A27 <dasahagun27@gmail.com>
2026-05-12 16:51:40 -0500

    new changes

--- scenes/Inventory/InventoryRec.gd
+++ scenes/Inventory/InventoryRec.gd
@@ -3,3 +3,15 @@ extends Resource
 class_name Inventory
 
 @export var slots: Array[InventorySlot]
+
+func insert(item: InventoryItem):
+	for slot in slots:
+		if slot.item == item:
+			slot.amount += 1
+			return
+			
+		for i in range(slots.size()):
+			if !slots[i].item:
+				slots[i].item = item
+				slots[i].amount = 1
+				return
--- scenes/Inventory/slot.gd
+++ scenes/Inventory/slot.gd
@@ -3,11 +3,11 @@ extends Panel
 @onready var backgroundSprite: Sprite2D = $Background
 @onready var itemSprite: Sprite2D = $CenterContainer/Panel/Item
 
-func update(item: InventoryItem):
-	if !item:
+func update(slot: InventorySlot):
+	if !slot.item:
 		backgroundSprite.frame = 0
 		itemSprite.visible = false
 	else:
 		backgroundSprite.frame = 1
 		itemSprite.visible = true
-		itemSprite.texture = item.texture
+		itemSprite.texture = slot.item.texture
