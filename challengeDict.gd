94f8abec46c9737b4c01efa50b8c1c9d00998087
Jeremy <jeremyorozco38@gmail.com>
2026-05-14 12:46:36 -0500

    Tweaked the challenge system
    added random static challenges in a dictionary that we can change, i created a checkifCompleted method, got the challenges to load in a panel along with the numbers that we want to track

---
+++ challengeDict.gd
@@ -0,0 +1,43 @@
+extends Resource
+
+var challengeUIFile = preload("res://scenes/challenge_ui.gd").new()
+
+#
+
+
+#here are the max to complete the challenges we can obviously increment the max
+var maxNumber1 = 10
+var maxNumber2 = 10
+var maxNumber3 = 100
+
+#dictionary of challenges that are basic
+var challenge = {
+	"challenge1" : {
+		"Desc": "Kill a number of mobs %s / %s" % [str(challengeUIFile.chal1Track), str(maxNumber1)],
+		"Completed": false,
+		"finished": preload("res://Screenshot 2026-05-13 201245.jpg")
+	},
+	"challenge2": {
+		"Desc": "Pick a certain number of plants %s / %s" % [str(challengeUIFile.chal2Track), str(maxNumber2)],
+		"Completed": false,
+		"finished": preload("res://Screenshot 2026-05-13 201245.jpg")
+	},
+	"challenge3": {
+		"Desc": "Pay your dues to the God or DIE %s" % [str(maxNumber3)],
+		"Completed": false,
+		"finished": preload("res://Screenshot 2026-05-13 201245.jpg")
+	} 
+}
+
+func updateMaxNumber(whichKeyChal: String) -> void:
+	
+	if whichKeyChal == "challenge1":
+		maxNumber1 += 10
+	elif whichKeyChal == "challenge2":
+		maxNumber2 += 10
+	elif whichKeyChal == "challenge3":
+		maxNumber3 += 100
+
+func completedChallenge(whichKeyChal: String) -> void:
+	challenge[whichKeyChal]["Completed"] = true
+	
