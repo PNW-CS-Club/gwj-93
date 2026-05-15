2a10b19b58d3535e18c000765f6cc94ab5ef15e7
Gunnar Dahl <73252935+gadahl@users.noreply.github.com>
2026-05-14 20:16:27 -0500

    unfinished seed planting

--- grid.gd
+++ grid.gd
@@ -5,24 +5,33 @@ class_name Grid extends Node2D
 # Plant -> Node
 # Debris -> Node
 
-@export var width = 5
-@export var height = 5
+@export var WIDTH = 5
+@export var HEIGHT = 5
+@export var COORD_OFFSET = Vector2i(0, 0)
 var grid = Array([], TYPE_OBJECT, "Node", null) #Array[Node]
 var plants: Array[Array] # Array of pairs that marks squares with living plants
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#initialize grid
-	for i in range(width * height):
+	for i in range(WIDTH * HEIGHT):
 		grid.append(null)
 
 # Access elements at (x,y).
 func at(x: int, y: int) -> Node: 
-	return grid[y * width + x]
+	return grid[y * WIDTH + x]
 
 func put(x: int, y: int, node: Node) -> void: 
-	grid[y * width + x] = node
+	grid[y * WIDTH + x] = node
 	if node.is_class("Plant"):
 		plants.append([x,y])
 
 # TODO: Change dead plants to debris
+
+
+
+func do_click(mouse_coords: Vector2i) -> void:
+	var coords = mouse_coords - COORD_OFFSET
+	# out-of-bounds check
+	if clampi(coords.x, 0, WIDTH-1) != coords.x || clampi(coords.y, 0, HEIGHT-1) != coords.y:
+		printerr("Cannot get tile at coordinates " + str(coords))
+		return
---
+++ scripts/farm_tile_layer.gd
@@ -0,0 +1,31 @@
+extends TileMapLayer
+
+@onready var highlight_marker: Sprite2D = %HighlightMarker
+@onready var grid: Grid = %Grid
+
+
+func _process(_delta: float) -> void:
+	if _get_tile_at_mouse():
+		# highlight map cell
+		var cell_pos: Vector2i = _get_coords_at_mouse()
+		highlight_marker.position = map_to_local(cell_pos)
+		highlight_marker.visible = true
+	else: 
+		highlight_marker.visible = false
+
+
+func _input(event: InputEvent) -> void:
+	var left_clicked = event is InputEventMouseButton && event.is_pressed() && event.button_index == MOUSE_BUTTON_LEFT
+	if !left_clicked: return
+	
+	if _get_tile_at_mouse(): 
+		var cell_pos: Vector2i = _get_coords_at_mouse()
+		print(cell_pos)
+		grid.do_click(cell_pos)
+
+
+func _get_coords_at_mouse() -> Vector2i:
+	return local_to_map(get_local_mouse_position())
+
+func _get_tile_at_mouse() -> TileData:
+	return get_cell_tile_data(_get_coords_at_mouse())
