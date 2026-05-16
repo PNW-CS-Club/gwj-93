96d89daa822f2091edd56924a906721881fc757f
Gunnar Dahl <73252935+gadahl@users.noreply.github.com>
2026-05-16 17:13:14 -0500

    fix hp plant, move boilerplate to Plant superclass

--- plants/buff/buff_plant.gd
+++ plants/buff/buff_plant.gd
@@ -1,13 +1,7 @@
 class_name BuffPlant extends Plant
 
-@onready var sprite: Sprite2D = $Sprite2D
-
 func _ready() -> void:
-	stats.level_changed.connect(_on_level_changed)
-	sprite.frame = stats.level - 1
-
-func _on_level_changed(level: int) -> void:
-	sprite.frame = level - 1
+	super._ready()
 
 func ability():
 	print("I'm Barry the Buff Plant!!")
