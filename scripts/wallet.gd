5f7f1e1a6baf4ab1c2823da59f4e792676b7d78b
Gunnar Dahl <73252935+gadahl@users.noreply.github.com>
2026-05-17 15:34:37 -0500

    fixed coin display UI, correct colors

--- scripts/wallet.gd
+++ scripts/wallet.gd
@@ -1,19 +1,15 @@
 class_name Wallet extends Control
 
-@onready var moneyLabel: Label = %moneyLabel
+@onready var coin_label: Label = %CoinLabel
 
 #our currency for the game
 var coins: int
 
 #the ready function will display the amount of coins on the screen
 func _ready() -> void:
-	
-	moneyLabel.text = str(coins)
-
+	coin_label.text = str(coins)
 
 #this method will subtract the amount that is purchased
-#this method is also being called in the shop_ui_test
 func change_balance(amount: int) -> void:
-	coins = coins + amount
-	moneyLabel.text = str(coins)
-	
+	coins += amount
+	coin_label.text = str(coins)
